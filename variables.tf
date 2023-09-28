5f25376430d1879243da19da12e787e7cd0acab7
Aj7Ay <110721907+Aj7Ay@users.noreply.github.com>
2023-09-28 14:17:39 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "ami_id" {
 variable "bucket_name" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "mrcloudbook-777-ajaykumar-yegireddi"
+  default     = "mrcloudbookaj-7-ajaykumar-yegireddi"
 }
 
 variable "acl" {
