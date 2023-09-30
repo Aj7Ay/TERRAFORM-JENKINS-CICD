2ffab7c0934acd85e7c80b4788804a5d25ff2f6e
Aj7Ay <110721907+Aj7Ay@users.noreply.github.com>
2023-09-30 10:53:11 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "mrcloudbook-cicd-bucket77-aj"
+    bucket         = "ajay-mrcloudbook777"
     key            = "my-terraform-environment/main"
     region         = "ap-south-1"
     dynamodb_table = "mrcloudbook-dynamo-db-table"
